5aa4631e7499440b50639c1e806425549de37e7a
Jan Willem Casteleijn <jw.ati44@gmail.com>
2017-10-13 23:32:20 +0200

    update
    Comments

--- src/ESP_8266_WS/src/main.cpp
+++ src/ESP_8266_WS/src/main.cpp
@@ -6,29 +6,36 @@
  *   version 1.0
  * ----------------------------------------------------------------------- */
 
-#include <ESP8266WiFi.h> 
-#include <ESP8266WiFiMulti.h> 
-#include <WebSocketsClient.h> 
-#include <Hash.h> 
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h>
+#include <Hash.h>
 #include <ArduinoJson.h>
-
-#define USE_SERIAL Serial1
 
 const char * IP_ADRESS = "192.168.0.110";
 const int PORT = 8080;
 const char * SSID = "DEVKEET";
 const char * PASSWD = "wifigratisbord";
 
+/*
+ * Enable ADC converter
+ * and SET VCC_IN Read = TRUE to measure Voltage level of the ESP8266
+ */
 ADC_MODE(ADC_VCC);
 
+/* Create instances of libraries */
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
+
+/* Initialize buffer for json data */
 StaticJsonBuffer < 200 > jsonBuffer;
 
 char buffer[256];
 char cmp[] = "whoareyou";
 float * vcc = 0;
 
+/* This function converts a typdef IPAddress to a string*/
+/* Also returns the string as s */
 String ipToString(IPAddress ip) {
   String s = "";
   for (int i = 0; i < 4; i++)
@@ -36,11 +43,13 @@ String ipToString(IPAddress ip) {
   return s;
 }
 
+/* get voltage level of the ESP8266 */
 uint32_t get_vcc() {
   uint32_t getVcc = ESP.getVcc();
   return getVcc;
 }
 
+/* Function whichs initializes the json data to send */
 void send_data() {
   JsonObject & json = jsonBuffer.createObject();
   JsonObject & jsonData = json.createNestedObject("data");
@@ -56,6 +65,7 @@ void send_data() {
   webSocket.sendTXT(buffer);
 }
 
+/* send data */
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   switch (type) {
   case WStype_DISCONNECTED:
@@ -64,7 +74,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     break;
   case WStype_TEXT:
     if (strcmp((const char * ) payload, cmp) == 0)
-      send_data();
+      send_data();    /* <-- sends data if server requests data */
     delay(1000);
     break;
   case WStype_BIN:
@@ -72,6 +82,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   }
 }
 
+/* Simple connect function */
 void connect_wifi() {
   WiFiMulti.addAP(SSID, PASSWD);
 
@@ -80,6 +91,7 @@ void connect_wifi() {
   }
 }
 
+/* Initialize websocket */
 void init_websocket() {
   webSocket.begin(IP_ADRESS, PORT, "/");
   webSocket.onEvent(webSocketEvent);
